9fea945f48cb36bbdecee5f4a266bcdfcc680c8d
Jonathan-Kayumov <72421906+Jonathan-Kayumov@users.noreply.github.com>
2020-11-08 17:13:28 -0500

    Jonatha assignment 00 01 (#2)

---
+++ Jonathan_assignment_01.js
@@ -0,0 +1,10 @@
+//Array of the months in a year by Jonathan K
+
+//making the array
+var months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+//this is a loop
+for (i = 0; i < months.length; i++) {
+    //list and array
+    console.log((i + 1) + ": " + months[i]);
+}
