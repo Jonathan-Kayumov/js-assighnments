741e4a887847b3d1f34481d81364a3ffe09b3d5f
Jonathan-Kayumov <jonathankayumova@gmail.com>
2020-11-15 14:01:29 -0500

    made a true false if else script

--- Jonathan_assignment_03.js
+++ Jonathan_assignment_03.js
@@ -1,5 +1,5 @@
 //loop command
-for (i = 20; i < 41; i++) {
+for (i = 1; i < 21; i++) {
     //20 is the starting nimber and 41 is what the loop will stop at
     //i++ is a shorter way of saying i=i+1
     console.log(i);
