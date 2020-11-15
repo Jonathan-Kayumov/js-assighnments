432f6f4e0aaa1b0edbb4b63cffcb15ee635a9889
Jonathan-Kayumov <jonathankayumova@gmail.com>
2020-11-15 14:40:29 -0500

    for loop cut off at 10 with break

---
+++ Jonathan_assignment_05.js
@@ -0,0 +1,6 @@
+for (i = 1; i < 21; i++) {    //loop command
+    console.log(i);          // console log runs loop command, must put it before break to print 10 first
+    if (i == 10) {           // break conditions
+        break                //break command
+    }
+}
