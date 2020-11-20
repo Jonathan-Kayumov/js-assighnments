649db23bd3e313ac8eba74b258c3b689e88045fc
Jonathan-Kayumov <jonathankayumova@gmail.com>
2020-11-20 12:36:44 -0500

    finished task but i think i made a mistake on the last one

--- Jonathan_assignment_07.js
+++ Jonathan_assignment_07.js
@@ -2,7 +2,7 @@ var shouldRun = true; //boolean var
 var i = 0;  //normal while variable
 while (shouldRun == true) { //while loo[]
     console.log(i);
-    if (i > 10) {
+    if (i == 10) {
         shouldRun = false;  //boolean false is i is greater than 10
     }
     i++;  //i+1
