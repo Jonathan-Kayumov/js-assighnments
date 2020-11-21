998e3e8eefda19ebf9cfe381cc3d7c10078cd63a
Jonathan-Kayumov <jonathankayumova@gmail.com>
2020-11-21 17:49:44 -0500

    js true false

---
+++ Jonathan_assignment_13.js
@@ -0,0 +1,9 @@
+function lang(js) {
+    if (js == "javascript") {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+}
+lang("javascript");
+lang("HTML");
--- Jonathan_assignments_12.js
+++ Jonathan_assignments_12.js
@@ -1,10 +1,7 @@
 function holiday(holidayName, dateStrring) {
     return "happy holiday " + holidayName + ',' + dateStrring;
 }
-
 var holFunc = holiday("Halloween", 'october 31st');
 console.log(holFunc);
-
 holFunc = holiday("Thanksgiven", 'november 25th');
 console.log(holFunc);
-
