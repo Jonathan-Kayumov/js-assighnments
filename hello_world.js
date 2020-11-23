544e0613fc92be13f0cbdc1d58adf2f0ea4dfaa1
Jonathan-Kayumov <72421906+Jonathan-Kayumov@users.noreply.github.com>
2020-11-23 17:17:28 -0500

    foobar (#18)

---
+++ hello_world.js
@@ -0,0 +1,13 @@
+//helloWorld application by Jonathan Kayumov
+
+
+//making it as a function
+
+function helloWorld() {
+    //intigrating the function action so it has something to do
+    console.log("Jonathan's version of Hello World but using a function");
+
+};
+
+//running the function
+helloWorld();
