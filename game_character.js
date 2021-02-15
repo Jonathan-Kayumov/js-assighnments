76518d782f3dfe58a78e89040075e8b2f0474ebe
Jonathan-Kayumov <jonathankayumova@gmail.com>
2021-02-15 18:15:45 -0500

    i need help with this new file

---
+++ game_character.js
@@ -0,0 +1,15 @@
+class genericGameCharacter {
+    constructor(name, type, attack, healthPoints) {
+        this.name = name;
+        this.type = type;
+        this.attack = attack;
+        this.healthPoints = healthPoints;
+    }
+    genericGameCharacterStats = () => {
+        console.log("The hero's name is " + this.name + ". " + this.name + "class is " + this.type + ". " + this.name + "DPS is " + this.attack + ". " + this.name + "HP is" + this.healthPoints + ". ");
+    }
+}
+
+let myCharacter = new character("Hanaqui", "crusader", "15", "105");
+
+myCharactere.genericGameCharacter();
