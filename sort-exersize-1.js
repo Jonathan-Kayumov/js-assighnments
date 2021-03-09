766391c9fdd750041f75f68ef5cd046a116c6b4f
Jonathan-Kayumov <jonathankayumova@gmail.com>
2021-03-09 16:23:12 -0500

    finished sorting exersize

--- sort-exersize-1.js
+++ sort-exersize-1.js
@@ -1,7 +1,7 @@
 let array1 = [5, 6, 4, 1, 15, 25];
-
+array1.sort((a, b) => a - b);
 console.log(array1);
 
 let array2 = ["cat", "dog", "phone", "apple", "new york", "bag"]
-
+array2.sort();
 console.log(array2);
